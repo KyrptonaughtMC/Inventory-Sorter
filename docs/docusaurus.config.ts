4c4f15f8972b455c3e3f7b7a601e21ebf109951e
meza <211977+meza@users.noreply.github.com>
2025-04-06 22:18:21 +0100

    docs: removed algolia - for now

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -63,13 +63,6 @@ const config: Config = {
         }
     ],
     themeConfig: {
-        algolia: {
-            appId: '9CN68M6PTQ',
-            apiKey: 'c04256a693ddd8deb649fcc774ced671',
-            indexName: 'Inventory Sorter-meza',
-            insights: true,
-            contextualSearch: true
-        },
         metadata: [
             {name: 'description', content: 'Inventory Sorter is a Gradle plugin that streamlines multi-loader, multi-version Minecraft mod development. Build Fabric, Forge, and NeoForge mods in a single codebase and simplify releases to Modrinth and CurseForge.'},
             {name: 'keywords', content: 'Minecraft modding, Gradle plugin, Fabric mod, Forge mod, NeoForge, multi-loader modding, Stonecutter, Architectury, Modrinth, CurseForge'},
