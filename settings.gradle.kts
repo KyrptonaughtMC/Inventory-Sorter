4c981d4304e0c2547ade8971699bb287099ec322
meza <211977+meza@users.noreply.github.com>
2026-06-26 10:28:35 +0100

    chore: update Stonecraft plugin version to 1.12.2 to use the latest modpublish plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
     }
 }
 plugins {
-    id("gg.meza.stonecraft") version "1.12.+"
+    id("gg.meza.stonecraft") version "1.12.2"
     id("dev.kikugie.stonecutter") version "0.9+"
 }
 
